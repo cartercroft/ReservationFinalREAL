47bd9e93d2f50912068049ca97639fc4a726916b
cartercroft <carterncroft@gmail.com>
2021-06-07 11:21:03 -0500

    Added IdentitySamples

--- ReservationFinal/ReservationFinal.MVC.UI/Startup.cs
+++ ReservationFinal/ReservationFinal.MVC.UI/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(ReservationFinal.MVC.UI.Startup))]
 namespace ReservationFinal.MVC.UI
 {
     public partial class Startup
