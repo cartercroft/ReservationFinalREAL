06d598627f46906df82b5c0f41fb810102840fba
cartercroft <carterncroft@gmail.com>
2021-06-08 15:20:09 -0500

    Added custom errors and unresolved error page. Need to style.

---
+++ ReservationFinal/ReservationFinal.MVC.UI/Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReservationFinal.MVC.UI.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public ActionResult Unresolved()
+        {
+            return View();
+        }
+    }
+}
