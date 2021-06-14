5f2d8761c7b29aadb7aec79e85a09cb1db2e5130
cartercroft <carterncroft@gmail.com>
2021-06-14 13:29:11 -0500

    Optimized Images, and link/scripts tags

--- ReservationFinal/ReservationFinal.MVC.UI/App_Start/BundleConfig.cs
+++ ReservationFinal/ReservationFinal.MVC.UI/App_Start/BundleConfig.cs
@@ -8,14 +8,26 @@ namespace ReservationFinal.MVC.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/js/bootstrap.min.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/css/styles.css",
-                      "~/Content/custom.css"));
+                      "~/Content/custom.css",
+                      "~/Content/css/jquery.dataTables.min.css",
+                      "~/Content/css/responsive.dataTables.min.css"
+                      ));
+
             bundles.Add(new ScriptBundle("~/Scripts/js").Include(
-                
+                "~/Scripts/js/bootstrap.bundle.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/Scripts/DataTables.js").Include(
+                "~/Scripts/js/jquery-ui.js",
+                "~/Scripts/js/jquery.dataTables.min.js",
+                "~/Scripts/js/dataTables.responsive.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/Scripts/Ajax.js").Include(
+                "~/Scripts/js/jquery.unobtrusive-ajax.min.js",
+                "~/Scripts/js/jquery-ui.js"
                 ));
         }
     }
